5d6960a87c3878793e25a7474a1a4c99dbfed165
AnujaSreeni <127194605+AnujaSreeni@users.noreply.github.com>
2024-02-27 15:59:40 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './Components/Navbar/Navbar';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
 import Hero from './Components/Hero/Hero';
 import View from './Components/View/View';
+import Buy from './Components/Buy/Buy';
 
 
 
@@ -20,6 +21,8 @@ function App() {
           <Route path='/login' element={<LoginSignup />} />
           <Route path='/s' element={<Hero />} />
           <Route path='/view/:id' element={<View />} />
+          <Route path='/buy/:id' element={<Buy />} />
+
 
         </Routes>
       </BrowserRouter>
--- src/Components/View/View.jsx
+++ src/Components/View/View.jsx
@@ -1,23 +1,71 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Button } from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import "./View.css";
+import { Buffer } from "buffer";
+import styled from "@emotion/styled";
+import { yellow } from "@mui/material/colors";
 
 const View = () => {
-    const [viewData, setViewData] = useState({})
-    const { id } = useParams()
+    const navigate = useNavigate()
 
-    useEffect(() => {
-        axios.get(`http://localhost:3005/view1/${id}`).then(({ data }) => {
-            setViewData(data)
-            console.log(data)
-        })
-    }, [])
 
-    return (
-        <div>
-            {viewData.Category}
-        </div>
-    )
-}
+    const handleClick = (id) => {
+  
+      console.log(id)
+      navigate(`/buy/${id}`)
+  
+    }
+  
 
-export default View
+  const [viewData, setViewData] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios.get(`http://localhost:3005/view1/${id}`).then(({ data }) => {
+      setViewData(data);
+      console.log(data.image1.data.data);
+    });
+  }, []);
+
+  return (
+    <div
+      style={{
+        padding: "1rem",
+        display: "flex",
+        gap: "1rem",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {/* <img src={`data:image/jpeg;base64,${Buffer.from(viewData.image1.data.data).toString('base64')}`}
+                                    width="50" height="50" alt="Error"/> */}
+
+      <div className="i">
+        {viewData.image1 && (
+          <img
+           
+            src={`data:image/jpeg;base64,${Buffer.from(
+              viewData.image1.data.data
+            ).toString("base64")}`}
+            width="200px"
+            height="auto"
+            alt="Error"
+          />
+        )}
+        <div className="u">&#8377;{viewData.Price}</div>
+
+        <Button onClick={() => handleClick(viewData._id)} variant="contained">Buy Now</Button>
+      </div>
+
+      <div className="p">
+        <h3>{viewData.Subcategory}</h3>
+        <p className="t">{viewData.Category}</p>
+        <p className="r">{viewData.Description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default View;
